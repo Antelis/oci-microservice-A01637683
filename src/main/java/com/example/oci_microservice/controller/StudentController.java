bc92f4242a799ca40923037822ec8ce505613157
Andres <bantelis10@gmail.com>
2025-03-09 20:04:39 -0600

    Students modified

---
+++ src/main/java/com/example/oci_microservice/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.oci_microservice.controller;
+
+import com.example.oci_microservice.model.Student;
+import com.example.oci_microservice.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+}
